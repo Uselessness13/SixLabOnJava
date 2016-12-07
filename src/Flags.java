0de2ce9d5f2cb981a9e2c55b1dbbf20594c521fc
Андрей Медведев < >
2016-12-07 21:08:15 +0300

    'firsttwo'

--- src/Flags.java
+++ src/Flags.java
@@ -1,7 +1,15 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 /**
  * Created by aaand on 07.12.2016.
  */
 public class Flags {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("ФЛАГИ");
+        frame.setVisible(true);
+    }
+    
 }
