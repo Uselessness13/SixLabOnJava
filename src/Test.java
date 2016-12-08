f65f42c54728ce32f78b2c6d9c2728ee3af2a3c3
Andrew <andrew13131336@gmail.com>
2016-12-08 21:50:06 +0300

    Shit

--- src/Test.java
+++ src/Test.java
@@ -1,39 +1,33 @@
+import javafx.scene.paint.*;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Color;
 import java.util.Scanner;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        JFrame frame=new JFrame("Test");
-        frame.setBounds(0, 0,700, 700);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame framec=new JFrame("Test");
+        framec.setBounds(0, 0,700, 700);
+        framec.setVisible(true);
+        framec.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel contentPane = new JPanel(){
             Graphics2D g2;
 
+            Object[] possibilities = {"Russia", "France", "Holland", "Belgium", "Italy"};
+            String s = (String)JOptionPane.showInputDialog(framec,"Choose your country than press OK","Choose ur country",JOptionPane.PLAIN_MESSAGE,null,possibilities,"Russia");
+
             protected void paintComponent(Graphics g){
                 super.paintComponent(g);
                 g2=(Graphics2D)g;
-                Scanner sc = new Scanner(System.in);
-                System.out.println("элула");
-                Integer numb = sc.nextInt();
-                switch (numb){
-                    case 1: {
-                        Flags glag = new Flags(100,100,200,400,1, Color.WHITE, Color.BLUE, Color.RED,g2, "Russia");
-                        break;
-                    }
-                    default: {
-                        System.out.println("Неверный формат");
-                        break;
-                    }
-                }
+                new Flags(100,100,500,700,1, Color.white, Color.blue, Color.red, g2, s);
 
             }
         };
-        frame.setContentPane(contentPane);
+        framec.setContentPane(contentPane);
 
     }
 
